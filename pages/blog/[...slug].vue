d67ce42f02ca930939b65b9f6d524389b43b3529
nei7 <franciszek.szarek2@gmail.com>
2025-02-13 19:56:47 +0100

    refactor: blog post

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -36,12 +36,10 @@ const { data, status } = await useAsyncData<ListBlockChildrenResponse & { post: 
 })
 
 const loadMoreTrigger = ref<HTMLDivElement>()
-const hasMore = ref(data.value?.has_more)
-const nextCursor = ref(data.value?.next_cursor)
 
 onMounted(() => {
 
-    // disable scroll resoration
+
     if ("history" in window) {
         window.history.scrollRestoration = 'auto';
     }
@@ -50,21 +48,21 @@ onMounted(() => {
 })
 
 const setupObserver = () => {
-    if (!loadMoreTrigger.value || status.value !== 'success' || !hasMore) return
+    if (!loadMoreTrigger.value || status.value !== 'success') return
 
     const observer = new IntersectionObserver(async (entries) => {
-        if (entries[0].isIntersecting && hasMore.value && data.value) {
+        if (entries[0].isIntersecting && data.value && data.value.next_cursor) {
 
-            const list = await fetchBlocks(data.value?.post.id, nextCursor.value ? nextCursor.value : undefined)
+            const list = await fetchBlocks(data.value?.post.id, data.value.next_cursor ? data.value.next_cursor : undefined)
             if (!list) {
-                hasMore.value = false
+                data.value.has_more = false
                 return
             }
 
             data.value.results.push(...list.results)
 
-            hasMore.value = list.has_more
-            nextCursor.value = list.next_cursor
+            data.value.has_more = list.has_more
+            data.value.next_cursor = list.next_cursor
         }
     }, {
         rootMargin: "200px",
@@ -81,16 +79,16 @@ const setupObserver = () => {
 
     <div class="mx-auto w-full max-w-4xl" v-if="data">
 
-        <div class="mt-12 lg:py-12">
+        <div class="mt-12 lg:py-12 px-4  ">
             <h1 class="text-4xl font-bold">{{ data.post.title }}</h1>
             <p class="text-muted-foreground mt-4">{{ useFormattedDate(data.post.createdAt) }}</p>
         </div>
 
-        <article class="mb-24">
+        <article class="mb-24 px-4 text-justify sm:text-start">
             <NotionRenderer :blocks="data.results"></NotionRenderer>
 
             <ClientOnly>
-                <div class="mt-10" ref="loadMoreTrigger" v-if="hasMore">
+                <div class="mt-10" ref="loadMoreTrigger" v-if="data.has_more">
                     Loading...
                 </div>
             </ClientOnly>
